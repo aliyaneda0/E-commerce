7fac587c182d85719063ae8c70e7e049d34291bf
aliya neda <137129762+aliyaneda0@users.noreply.github.com>
2026-01-03 15:16:18 +0530

    add functionality to fetch all the products
    Signed-off-by: aliya neda <137129762+aliyaneda0@users.noreply.github.com>

--- src/main/java/com/aliya/E_commerce/controller/ProductController.java
+++ src/main/java/com/aliya/E_commerce/controller/ProductController.java
@@ -1,14 +1,24 @@
 package com.aliya.E_commerce.controller;
 
 import com.aliya.E_commerce.model.Product;
+import com.aliya.E_commerce.service.ProductService;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/products")
 @CrossOrigin("*")
 public class ProductController {
 
-List<Product>getAllProducts()
+
+    private ProductService productService;
+
+ @GetMapping
+ List<Product> getAllProducts(){
+     return productService.getAllProducts();
+ }
 }
--- src/main/java/com/aliya/E_commerce/service/ProductService.java
+++ src/main/java/com/aliya/E_commerce/service/ProductService.java
@@ -1,4 +1,18 @@
 package com.aliya.E_commerce.service;
 
+import com.aliya.E_commerce.model.Product;
+import com.aliya.E_commerce.repo.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
 }
